afd8cc1b36ebeb1cd9e7a00a3369e0e5efc1a5a0
Simon Levesque <simon@simonlevesque.com>
2017-10-27 18:53:09 -0400

    Fix docker upload

--- step-upload-docker-image.sh
+++ step-upload-docker-image.sh
@@ -6,6 +6,7 @@ RUN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $RUN_PATH
 
 echo ----[ Upload docker image ]----
+DOCKER_IMAGE=replace-php-serialize-safe:$VERSION
 docker login
 docker tag $DOCKER_IMAGE foilen/$DOCKER_IMAGE
 docker tag $DOCKER_IMAGE foilen/replace-php-serialize-safe:latest
